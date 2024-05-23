4465012d224d29f296892cafb6c576485cbb0dd7
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-05-23 22:08:01 +0530

    complete fist react effect app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,39 @@
+/* eslint-disable react/prop-types */
+import { useEffect } from "react";
+import { useState } from "react";
+
 export default function App () {
-  return <div>
+
+  //use state
+  const [message , setMessage] = useState("");
+  const [ count , setCount] = useState(0);
+  //make a data fetching function
+  async function getAdvice() {
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = res.json();
+    setMessage(data);
+    setCount ((c) => c + 1);
+  }
+
+  useEffect( function (){
+    getAdvice();
+  } , [])
+
+  return (<div>
       <h1>Hello World</h1>
-      <button>Get Advice</button>
-  </div>
+      <button onClick={getAdvice}>Get Advice</button>
+      <br />
+      <Message count={count} message = {message} />
+  </div>);
 }
+
+
+function Message(props) {
+  return(
+    <div>
+      <p>{props.message}</p>
+      <h2>Count : {props.count}</h2>
+    </div>
+  );
+}
+
